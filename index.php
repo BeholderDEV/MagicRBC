1d0e7723127932bb457ce0deee0b5060c29fda8d
AlissonSteffens <alisson.steffens@hotmail.com>
2017-04-26 21:27:07 -0300

    index test

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Parking GPS</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/estilo.css"></link>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="jumbotron"><h1><img class="car" src="img/parking.png"> Parking GPS</h1></div>
+
+      <a href="cadastro_veiculo.php" class="btn btn-primary">Cadastro de Veículo</a>
+
+      <a href="enviar_ponto.php" class="btn btn-primary">Enviar Ponto</a>
+
+      <a href="ver_movimentacoes.php" class="btn btn-primary">Ver Movimentação</a>
+
+      <a href="estacionamentos_proximos.php" class="btn btn-primary">Ver Estacionamentos Próximos</a>
+
+      <a href="ver_carros.php" class="btn btn-primary">Lista de Carros</a>
+    </div> <!-- /container -->
+  </body>
+</html>
